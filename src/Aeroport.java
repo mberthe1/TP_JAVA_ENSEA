c1fddfa2c5ec00f58a722396b68d4df9c15f8475
berthem <mathilda.berthe@ensea.fr>
2022-01-13 10:12:49 +0100

    test

--- src/Aeroport.java
+++ src/Aeroport.java
@@ -1,4 +1,38 @@
-public class Aeroport {
-    //test
-    //tetete
+public class Aeroport
+   {
+    private String IATA;
+    private String Name;
+    private String country;
+    private Double latitude;
+    private Double longitude;
+
+    public Aeroport (String IATA, String Name, String country, Double latitude, Double longitude){
+        this.IATA = IATA;
+        this.Name = Name;
+        this.country = country;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getIATA()
+    {
+        return IATA;
+    }
+
+    public Double getLatitude()
+    {
+        return latitude;
+    }
+
+    public Double getLongitude()
+    {
+        return longitude;
+    }
+
+    @Override
+    public String toString()
+    {
+        return ("L'aéroport est " + Name + "\n" + "Le IATA est " + IATA + "\n" + "Le nom est " + Name + "\n"
+                + "Le pays est " + country + "\n" + "La latitude est " + latitude + "\n" + "La longétude est " + longitude + "\n");
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    static public void main(String args[])
+    {
+        Aeroport CharlesDeGaulle = new Aeroport("CDG", "Charles de Gaulle", "France", 49.0, 2.5);
+        System.out.println(CharlesDeGaulle);
+    }
+}
