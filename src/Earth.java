fe6a4a0ea6f7ec1f6c835c5ae5dd57f692e94574
berthem <mathilda.berthe@ensea.fr>
2022-01-21 10:51:34 +0100

    test

--- src/Earth.java
+++ src/Earth.java
@@ -1,3 +1,39 @@
-public class Earth {
+import javafx.animation.AnimationTimer;
+import javafx.geometry.Point3D;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.image.Image;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
 
+import java.util.ArrayList;
+
+public class Earth extends Group {
+
+    private Rotate ry = new Rotate(0, new Point3D(0,1,0));
+    private Sphere sph = new Sphere();
+    ArrayList<Sphere> yellowSphere = new ArrayList<>();
+    PhongMaterial material = new PhongMaterial();
+    //private Scene ihm = new Scene();
+
+    public Earth() {
+        super();
+        Image im = new Image("/earth_lights_4800.png");
+        sph.setRadius(300);
+        sph.getTransforms().add(ry);
+        material.setDiffuseMap(im);
+        sph.setMaterial(material);
+        this.getChildren().add(sph);
+        animationTimer.start();
+
+
+    }
+    AnimationTimer animationTimer = new AnimationTimer() {
+        double angle = 0;
+        @Override
+        public void handle(long time) {
+            ry.setAngle(time/50000000);
+        }
+    };
 }
--- src/Interface.java
+++ src/Interface.java
@@ -1,20 +1,72 @@
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.PickResult;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Interface extends Application {
+    double gety;
+    double getx;
+    double zoom;
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Hello world");
-        Group root = new Group();
-        Pane pane = new Pane(root);
-        Scene theScene = new Scene(pane, 600, 400,true);
-        primaryStage.setScene(theScene);
+        Earth earth = new Earth();
+        Pane pane = new Pane(earth);
+        Scene ihm = new Scene(pane, 600, 400, true);
+        primaryStage.setScene(ihm);
         primaryStage.show();
+        //  Event event = new Event();
+
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setTranslateZ(-1500);
+        camera.setNearClip(0.1);
+        camera.setFarClip(2000.0);
+        camera.setFieldOfView(40);
+        ihm.setCamera(camera);
+
+        ihm.addEventHandler(MouseEvent.ANY, event -> {
+
+            if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+                gety = event.getSceneY();
+                getx = event.getSceneX();
+                System.out.println("Clicked on : (" + event.getSceneX() + ", " + event.getSceneY() + ")");
+            }
+
+            if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+                zoom = gety - event.getSceneY();
+                camera.setTranslateZ(-1500 + zoom);
+            }
+
+            if (event.getButton()== MouseButton.SECONDARY && event.getEventType()==MouseEvent.MOUSE_CLICKED) {
+                PickResult pickResult = event.getPickResult();
+
+                double lon = 0.0;
+                double lat = 0.0;
+                Aeroport near;
+                if (pickResult.getIntersectedNode() != null) {
+                    getx = pickResult.getIntersectedPoint().getX();
+                    gety = pickResult.getIntersectedPoint().getY();
+                    lon = 360 * (getx - 0.5);
+                    lat = Math.toDegrees(Math.atan(Math.exp((0.5 - gety) / 0.2678) - Math.PI / 2));
+                    System.out.println("longitude =" + lon + " latitude =" + lat);
+                    World w = new World("airport-codes_no_comma.csv");
+                    near = w.findNearestAirport(lon, lat);
+                    System.out.println(near);
+
+                }
+            }
+
+        });
+
     }
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch(args);
+
     }
 }
